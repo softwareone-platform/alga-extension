062879d6f29445559f2ee9508d34b43fe3141aed
Jacek Kruzel <jacek.kruzel@gmail.com>
2025-09-06 17:50:14 +0200

    312

--- src/app/settings/details/details.tsx
+++ src/app/settings/details/details.tsx
@@ -1,41 +1,53 @@
+import { useExtensionDetails } from "@features/extension";
 import { Card } from "@ui/card";
+import dayjs from "dayjs";
 
 export function Details() {
+  const { details } = useExtensionDetails();
+
   return (
     <Card>
       <div>
         <label className="block text-sm font-semibold text-gray-900">
           Created
         </label>
-        <div className="text-sm text-gray-500">-</div>
+        <div className="text-sm text-gray-500">
+          {dayjs(details.createdAt).format("HH:mm MM/DD/YYYY") || "-"}
+        </div>
       </div>
 
       <div>
         <label className="block text-sm font-semibold text-gray-900">
           Updated
         </label>
-        <div className="text-sm text-gray-500">-</div>
+        <div className="text-sm text-gray-500">
+          {dayjs(details.updatedAt).format("HH:mm MM/DD/YYYY") || "-"}
+        </div>
       </div>
 
       <div>
         <label className="block text-sm font-semibold text-gray-900">
           Activated
         </label>
-        <div className="text-sm text-gray-500">-</div>
+        <div className="text-sm text-gray-500">
+          {dayjs(details.activatedAt).format("HH:mm MM/DD/YYYY") || "-"}
+        </div>
       </div>
 
       <div>
         <label className="block text-sm font-semibold text-gray-900">
           Disabled
         </label>
-        <div className="text-sm text-gray-500">-</div>
+        <div className="text-sm text-gray-500">
+          {dayjs(details.disabledAt).format("HH:mm MM/DD/YYYY") || "-"}
+        </div>
       </div>
 
       <div>
         <label className="block text-sm font-semibold text-gray-900">
           Note
         </label>
-        <div className="text-sm text-gray-500">-</div>
+        <div className="text-sm text-gray-500">{details.note || "-"}</div>
       </div>
     </Card>
   );
