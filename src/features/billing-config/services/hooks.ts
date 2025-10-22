2cedfe5c6e1b26d60c271daae96442d4b870a7f0
Jacek Kruzel <jacek.kruzel@gmail.com>
2025-10-22 13:55:37 +0200

    21

--- src/features/billing-config/services/hooks.ts
+++ src/features/billing-config/services/hooks.ts
@@ -1,14 +1,34 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { BillingConfigsContext } from "./context";
-import { BillingConfigChanges } from "@lib/alga";
+import { BillingConfig, BillingConfigChanges } from "@lib/alga";
 
 export const useBillingConfigsByAgreements = (agreementsIds: string[]) => {
   const { client } = useContext(BillingConfigsContext);
+  const queryClient = useQueryClient();
+
+  const getById = useCallback(
+    async (id: string) => {
+      const cached = queryClient.getQueryData<BillingConfig>([
+        "billing-configs",
+        id,
+      ]);
+      if (cached) return cached;
+
+      const result = await client!.getByAgreementId(id);
+      if (result) queryClient.setQueryData(["billing-configs", id], result);
+
+      return result;
+    },
+    [queryClient]
+  );
 
   const { data: billingConfigs, ...state } = useQuery({
     queryKey: ["billing-configs", agreementsIds],
-    queryFn: () => client!.getByAgreementsIds(agreementsIds),
+    queryFn: async () => {
+      const results = await Promise.all(agreementsIds.map(getById));
+      return results.filter((result) => !!result);
+    },
     enabled: !!client,
   });
 
@@ -22,6 +42,7 @@ export const useBillingConfigsByConsumer = (consumerId: string) => {
     queryKey: ["billing-configs", consumerId],
     queryFn: () => client!.getByConsumerId(consumerId),
     enabled: !!client,
+    staleTime: Infinity,
   });
 
   return { billingConfigs, ...state };
@@ -34,6 +55,7 @@ export const useBillingConfig = (agreementId?: string) => {
     queryKey: ["billing-configs", agreementId],
     queryFn: () => client!.getByAgreementId(agreementId!),
     enabled: !!client && !!agreementId,
+    staleTime: Infinity,
   });
 
   return { billingConfig, ...state };
