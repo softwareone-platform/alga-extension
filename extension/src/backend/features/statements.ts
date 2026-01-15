d34c0f559194cdd298f3ee2c2184b3590434bc6f
Jacek Kruzel <jacek.kruzel@gmail.com>
2026-01-15 12:58:16 +0100

    wq

--- extension/src/backend/features/statements.ts
+++ extension/src/backend/features/statements.ts
@@ -2,55 +2,54 @@ import { storage } from "../lib/alga/storage";
 import { Statement } from "@/shared/statements";
 import {
   Charge,
+  ChargeListResponse,
   StatementListResponse,
   Statement as SWOStatement,
 } from "@swo/mp-api-model/billing";
 import { BillingConfig } from "@/shared/billing-configs";
 import { billingConfigs } from "./billing-configs";
-import { ManualInvoice, ManualInvoiceLine } from "../lib/alga";
-import { StatementsClient } from "../lib/swo";
-import { extension } from "./extension";
 import { SWOClient } from "../lib/swo/client";
 import { ListResponse } from "../lib/swo/api";
-import { AgreementListResponse } from "@swo/mp-api-model/platform/models/AgreementListResponse";
 
 const STORAGE_NAMESPACE = "swo.statements";
 const STORAGE_KEY = "all";
+const STATEMENTS_LIMIT = 100;
+const CHARGES_LIMIT = 100;
 
 type InvoiceData = {
   invoiceId: string;
   markup: number;
 };
 
-const toLine = (
-  charge: Charge,
-  billingConfig: BillingConfig,
-): ManualInvoiceLine | null => {
-  if (!charge.price?.unitSP) {
-    console.warn(`No price for charge ${charge.id}`);
-    return null;
-  }
+// const toLine = (
+//   charge: Charge,
+//   billingConfig: BillingConfig
+// ): ManualInvoiceLine | null => {
+//   if (!charge.price?.unitSP) {
+//     console.warn(`No price for charge ${charge.id}`);
+//     return null;
+//   }
 
-  const description = `${charge.item?.name || charge.description?.value1} (${
-    charge.id
-  })`;
+//   const description = `${charge.item?.name || charge.description?.value1} (${
+//     charge.id
+//   })`;
 
-  const rate = Math.round(
-    charge.price.unitSP * (1 + billingConfig.markup / 100) * 100,
-  );
+//   const rate = Math.round(
+//     charge.price.unitSP * (1 + billingConfig.markup / 100) * 100
+//   );
 
-  return {
-    serviceId: billingConfig.serviceId,
-    quantity: charge.quantity ?? 0,
-    description,
-    rate,
-  } satisfies ManualInvoiceLine;
-};
+//   return {
+//     serviceId: billingConfig.serviceId,
+//     quantity: charge.quantity ?? 0,
+//     description,
+//     rate,
+//   } satisfies ManualInvoiceLine;
+// };
 
 const toStatement = (
   swoStatement: SWOStatement,
   invoiceData?: InvoiceData,
-  billingConfig?: BillingConfig,
+  billingConfig?: BillingConfig
 ): Statement => {
   if (!billingConfig) {
     return {
@@ -90,25 +89,25 @@ export class StatementService {
   getById(id: string, rql: string): Statement {
     const statement = this.swoClient.fetch<SWOStatement>(
       `/billing/statements/${id}`,
-      rql,
+      rql
     );
 
     const invoicesData =
       storage.get<Record<string, InvoiceData>>(
         STORAGE_NAMESPACE,
-        STORAGE_KEY,
+        STORAGE_KEY
       ) ?? {};
 
     const bcs = billingConfigs.getConfigs();
 
     const billingConfig = bcs.find(
-      (bc) => bc.agreementId === statement.agreement?.id,
+      (bc) => bc.agreementId === statement.agreement?.id
     );
 
     return toStatement(
       statement,
       invoicesData[statement.id ?? ""],
-      billingConfig,
+      billingConfig
     );
   }
 
@@ -123,25 +122,22 @@ export class StatementService {
     const invoicesData =
       storage.get<Record<string, InvoiceData>>(
         STORAGE_NAMESPACE,
-        STORAGE_KEY,
+        STORAGE_KEY
       ) ?? {};
 
     const bcs = billingConfigs.getConfigs();
 
-    const billingConfigsByAgreementId = bcs.reduce(
-      (acc, config) => {
-        acc[config.agreementId] = config;
-        return acc;
-      },
-      {} as Record<string, BillingConfig>,
-    );
+    const billingConfigsByAgreementId = bcs.reduce((acc, config) => {
+      acc[config.agreementId] = config;
+      return acc;
+    }, {} as Record<string, BillingConfig>);
 
     const statements = swoStatements.map((swoStatement) =>
       toStatement(
         swoStatement,
         invoicesData[swoStatement.id ?? ""],
-        billingConfigsByAgreementId[swoStatement.agreement?.id ?? ""],
-      ),
+        billingConfigsByAgreementId[swoStatement.agreement?.id ?? ""]
+      )
     );
 
     return { data: statements, $meta };
@@ -150,7 +146,7 @@ export class StatementService {
   invoiceStatement(statementId: string): Statement {
     const swoStatement = this.swoClient.fetch<SWOStatement>(
       `/billing/statements/${statementId}`,
-      "",
+      "select=id,agreement.id"
     );
 
     const billingConfig = billingConfigs
@@ -158,20 +154,29 @@ export class StatementService {
       .find((bc) => bc.agreementId === swoStatement.agreement?.id);
     if (!billingConfig) {
       throw new Error(
-        `Billing config not found for agreement ${swoStatement.agreement?.id}`,
+        `Billing config not found for agreement ${swoStatement.agreement?.id}`
       );
     }
 
     let invoicesData =
       storage.get<Record<string, InvoiceData>>(
         STORAGE_NAMESPACE,
-        STORAGE_KEY,
+        STORAGE_KEY
       ) ?? {};
     if (invoicesData[statementId]) {
       throw new Error(`Invoice already exists for statement ${statementId}`);
     }
 
     //TODO: Create invoice in Alga
+    const charges = this.fetchAllCharges(swoStatement.id!);
+    if (charges.length === 0) {
+      throw new Error(`No charges found for statement ${statementId}`);
+    }
+    //     const lines = charges
+    //       .map((charge) => toLine(charge, billingConfig))
+    //       .filter((line) => !!line);
+    //     console.log(lines);
+    //     const manualInvoice = {} as ManualInvoice; // MOCKED
     invoicesData[statementId] = {
       invoiceId: "SOME_ID",
       markup: billingConfig.markup,
@@ -183,94 +188,86 @@ export class StatementService {
   }
 
   invoiceAll(): void {
-    const { data: swoStatements, $meta } =
-      this.swoClient.fetch<StatementListResponse>("/billing/statements", "rql");
-
     const bcs = billingConfigs.getConfigs();
-    const billingConfigsByAgreementId = bcs.reduce(
-      (acc, config) => {
-        acc[config.agreementId] = config;
-        return acc;
-      },
-      {} as Record<string, BillingConfig>,
-    );
+    const billingConfigsByAgreementId = bcs.reduce((acc, config) => {
+      acc[config.agreementId] = config;
+      return acc;
+    }, {} as Record<string, BillingConfig>);
 
     const invoicesData =
       storage.get<Record<string, InvoiceData>>(
         STORAGE_NAMESPACE,
-        STORAGE_KEY,
+        STORAGE_KEY
       ) ?? {};
 
-    for (const swoStatement of swoStatements ?? []) {
-      const billingConfig =
-        billingConfigsByAgreementId[swoStatement.agreement?.id ?? ""];
-      if (!billingConfig) {
-        continue;
+    let offset = 0;
+
+    while (true) {
+      const { data: swoStatements, $meta } =
+        this.swoClient.fetch<StatementListResponse>(
+          "/billing/statements",
+          `[MAGIC!!!!!]&offset=${offset}&limit=${STATEMENTS_LIMIT}`
+        );
+      if (($meta?.pagination?.total ?? 0) <= offset) {
+        break;
       }
 
-      const invoiceData = invoicesData[swoStatement.id!];
-      if (invoiceData) {
-        continue;
+      for (const swoStatement of swoStatements!) {
+        const billingConfig =
+          billingConfigsByAgreementId[swoStatement.agreement?.id ?? ""];
+        if (!billingConfig) {
+          continue;
+        }
+
+        const invoiceData = invoicesData[swoStatement.id!];
+        if (invoiceData) {
+          continue;
+        }
+
+        //TODO: Create invoice in Alga
+        const charges = this.fetchAllCharges(swoStatement.id!);
+        if (charges.length === 0) {
+          continue;
+        }
+        // const lines = charges
+        //   .map((charge) => toLine(charge, billingConfig))
+        //   .filter((line) => !!line);
+        // console.log(lines);
+        // const manualInvoice = {} as ManualInvoice; // MOCKED
+        invoicesData[swoStatement.id!] = {
+          invoiceId: "SOME_ID",
+          markup: billingConfig.markup,
+        };
       }
+
+      offset += STATEMENTS_LIMIT;
     }
   }
 
-  // createInvoices(statements: SWOStatement[]) {
-  //   const bcs = billingConfigs.getConfigs();
-  //   const billingConfigsByAgreementId = bcs.reduce((acc, config) => {
-  //     acc[config.agreementId] = config;
-  //     return acc;
-  //   }, {} as Record<string, BillingConfig>);
+  private fetchAllCharges(statementId: string): Charge[] {
+    let offset = 0;
+    const charges: Charge[] = [];
 
-  //   const algaEntries =
-  //     storage.get<Record<string, InvoiceData>>(
-  //       STORAGE_NAMESPACE,
-  //       STORAGE_KEY
-  //     ) || {};
+    while (true) {
+      const response = this.swoClient.fetch<ChargeListResponse>(
+        `/billing/statements/${statementId}/charges`,
+        `select=id,subscription.id,subscription.name,item.id,item.name,period.start,period.end,quantity,price.SPx1,price.unitSP&offset=${offset}&limit=${CHARGES_LIMIT}`
+      );
+      if (!response) {
+        throw new Error(`Failed to fetch charges.`);
+      }
 
-  //   for (const swoStatement of statements) {
-  //     const billingConfig = swoStatement.agreement?.id
-  //       ? billingConfigsByAgreementId[swoStatement.agreement.id]
-  //       : null;
+      const { data, $meta } = response!;
+      charges.push(...(data || []));
 
-  //     if (
-  //       !billingConfig ||
-  //       !billingConfig.serviceId ||
-  //       billingConfig.markup === undefined
-  //     ) {
-  //       // not billable
-  //       continue;
-  //     }
+      offset += CHARGES_LIMIT;
 
-  //     const algaEntry = algaEntries[swoStatement.id!];
-  //     if (!!algaEntry) {
-  //       // already invoiced
-  //       continue;
-  //     }
+      const total = $meta?.pagination?.total || 0;
+      if (total <= offset) {
+        break;
+      }
+    }
 
-  //     const { endpoint, token } = extension.getDetails();
-  //     const swoClient = new SWOClient(endpoint, token);
-  //     const statementsClient = new StatementsClient(swoClient);
-
-  //     const charges = statementsClient.getCharges(swoStatement.id!);
-  //     if (charges.length === 0) {
-  //       // no charges
-  //       continue;
-  //     }
-
-  //     const lines = charges
-  //       .map((charge) => toLine(charge, billingConfig))
-  //       .filter((line) => !!line);
-  //     console.log(lines);
-  //     const manualInvoice = {} as ManualInvoice; // MOCKED
-
-  //     const newInvoiceData = {
-  //       invoiceId: manualInvoice.invoiceId,
-  //       markup: billingConfig.markup,
-  //     } satisfies InvoiceData;
-
-  //     algaEntries[swoStatement.id!] = newInvoiceData;
-  //     storage.put(STORAGE_NAMESPACE, STORAGE_KEY, algaEntries);
-  //   }
-  // }
+    return charges;
+  }
 }
