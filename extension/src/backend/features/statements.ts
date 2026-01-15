7293d84ecd63eacfd4aa646c88349961157c9580
Jacek Kruzel <jacek.kruzel@gmail.com>
2026-01-15 12:33:39 +0100

    Update statements.ts

--- extension/src/backend/features/statements.ts
+++ extension/src/backend/features/statements.ts
@@ -12,6 +12,7 @@ import { StatementsClient } from "../lib/swo";
 import { extension } from "./extension";
 import { SWOClient } from "../lib/swo/client";
 import { ListResponse } from "../lib/swo/api";
+import { AgreementListResponse } from "@swo/mp-api-model/platform/models/AgreementListResponse";
 
 const STORAGE_NAMESPACE = "swo.statements";
 const STORAGE_KEY = "all";
@@ -48,13 +49,9 @@ const toLine = (
 
 const toStatement = (
   swoStatement: SWOStatement,
-  invoicesData: Record<string, InvoiceData>,
-  billingConfigs: Record<string, BillingConfig>,
+  invoiceData?: InvoiceData,
+  billingConfig?: BillingConfig,
 ): Statement => {
-  const billingConfig = swoStatement.agreement?.id
-    ? billingConfigs[swoStatement.agreement.id]
-    : null;
-
   if (!billingConfig) {
     return {
       ...swoStatement,
@@ -64,7 +61,6 @@ const toStatement = (
     };
   }
 
-  const invoiceData = invoicesData[swoStatement.id!];
   if (!invoiceData) {
     return {
       ...swoStatement,
@@ -105,15 +101,15 @@ export class StatementService {
 
     const bcs = billingConfigs.getConfigs();
 
-    const billingConfigsByAgreementId = bcs.reduce(
-      (acc, config) => {
-        acc[config.agreementId] = config;
-        return acc;
-      },
-      {} as Record<string, BillingConfig>,
+    const billingConfig = bcs.find(
+      (bc) => bc.agreementId === statement.agreement?.id,
     );
 
-    return toStatement(statement, invoicesData, billingConfigsByAgreementId);
+    return toStatement(
+      statement,
+      invoicesData[statement.id ?? ""],
+      billingConfig,
+    );
   }
 
   getByRQL(rql: string): ListResponse<Statement> {
@@ -141,13 +137,55 @@ export class StatementService {
     );
 
     const statements = swoStatements.map((swoStatement) =>
-      toStatement(swoStatement, invoicesData, billingConfigsByAgreementId),
+      toStatement(
+        swoStatement,
+        invoicesData[swoStatement.id ?? ""],
+        billingConfigsByAgreementId[swoStatement.agreement?.id ?? ""],
+      ),
     );
 
     return { data: statements, $meta };
   }
 
-  createInvoices(statements: SWOStatement[]) {
+  invoiceStatement(statementId: string): Statement {
+    const swoStatement = this.swoClient.fetch<SWOStatement>(
+      `/billing/statements/${statementId}`,
+      "",
+    );
+
+    const billingConfig = billingConfigs
+      .getConfigs()
+      .find((bc) => bc.agreementId === swoStatement.agreement?.id);
+    if (!billingConfig) {
+      throw new Error(
+        `Billing config not found for agreement ${swoStatement.agreement?.id}`,
+      );
+    }
+
+    let invoicesData =
+      storage.get<Record<string, InvoiceData>>(
+        STORAGE_NAMESPACE,
+        STORAGE_KEY,
+      ) ?? {};
+    if (invoicesData[statementId]) {
+      throw new Error(`Invoice already exists for statement ${statementId}`);
+    }
+
+    //TODO: Create invoice in Alga
+    invoicesData[statementId] = {
+      invoiceId: "SOME_ID",
+      markup: billingConfig.markup,
+    };
+
+    storage.put(STORAGE_NAMESPACE, STORAGE_KEY, invoicesData);
+
+    return toStatement(swoStatement, invoicesData[statementId], billingConfig);
+  }
+
+  invoiceAll(): void {
+    const { data: swoStatements, $meta } =
+      this.swoClient.fetch<StatementListResponse>("/billing/statements", "rql");
+
     const bcs = billingConfigs.getConfigs();
     const billingConfigsByAgreementId = bcs.reduce(
       (acc, config) => {
@@ -157,55 +195,82 @@ export class StatementService {
       {} as Record<string, BillingConfig>,
     );
 
-    let algaEntries =
+    const invoicesData =
       storage.get<Record<string, InvoiceData>>(
         STORAGE_NAMESPACE,
         STORAGE_KEY,
-      ) || {};
+      ) ?? {};
 
-    for (const swoStatement of statements) {
-      const billingConfig = swoStatement.agreement?.id
-        ? billingConfigsByAgreementId[swoStatement.agreement.id]
-        : null;
-
-      if (
-        !billingConfig ||
-        !billingConfig.serviceId ||
-        billingConfig.markup === undefined
-      ) {
-        // not billable
+    for (const swoStatement of swoStatements ?? []) {
+      const billingConfig =
+        billingConfigsByAgreementId[swoStatement.agreement?.id ?? ""];
+      if (!billingConfig) {
         continue;
       }
 
-      const algaEntry = algaEntries[swoStatement.id!];
-      if (!!algaEntry) {
-        // already invoiced
+      const invoiceData = invoicesData[swoStatement.id!];
+      if (invoiceData) {
         continue;
       }
-
-      const { endpoint, token } = extension.getDetails();
-      const swoClient = new SWOClient(endpoint, token);
-      const statementsClient = new StatementsClient(swoClient);
-
-      const charges = statementsClient.getCharges(swoStatement.id!);
-      if (charges.length === 0) {
-        // no charges
-        continue;
-      }
-
-      const lines = charges
-        .map((charge) => toLine(charge, billingConfig))
-        .filter((line) => !!line);
-      console.log(lines);
-      const manualInvoice = {} as ManualInvoice; // MOCKED
-
-      const newInvoiceData = {
-        invoiceId: manualInvoice.invoiceId,
-        markup: billingConfig.markup,
-      } satisfies InvoiceData;
-
-      algaEntries[swoStatement.id!] = newInvoiceData;
-      storage.put(STORAGE_NAMESPACE, STORAGE_KEY, algaEntries);
     }
   }
+
+  // createInvoices(statements: SWOStatement[]) {
+  //   const bcs = billingConfigs.getConfigs();
+  //   const billingConfigsByAgreementId = bcs.reduce((acc, config) => {
+  //     acc[config.agreementId] = config;
+  //     return acc;
+  //   }, {} as Record<string, BillingConfig>);
+
+  //   const algaEntries =
+  //     storage.get<Record<string, InvoiceData>>(
+  //       STORAGE_NAMESPACE,
+  //       STORAGE_KEY
+  //     ) || {};
+
+  //   for (const swoStatement of statements) {
+  //     const billingConfig = swoStatement.agreement?.id
+  //       ? billingConfigsByAgreementId[swoStatement.agreement.id]
+  //       : null;
+
+  //     if (
+  //       !billingConfig ||
+  //       !billingConfig.serviceId ||
+  //       billingConfig.markup === undefined
+  //     ) {
+  //       // not billable
+  //       continue;
+  //     }
+
+  //     const algaEntry = algaEntries[swoStatement.id!];
+  //     if (!!algaEntry) {
+  //       // already invoiced
+  //       continue;
+  //     }
+
+  //     const { endpoint, token } = extension.getDetails();
+  //     const swoClient = new SWOClient(endpoint, token);
+  //     const statementsClient = new StatementsClient(swoClient);
+
+  //     const charges = statementsClient.getCharges(swoStatement.id!);
+  //     if (charges.length === 0) {
+  //       // no charges
+  //       continue;
+  //     }
+
+  //     const lines = charges
+  //       .map((charge) => toLine(charge, billingConfig))
+  //       .filter((line) => !!line);
+  //     console.log(lines);
+  //     const manualInvoice = {} as ManualInvoice; // MOCKED
+
+  //     const newInvoiceData = {
+  //       invoiceId: manualInvoice.invoiceId,
+  //       markup: billingConfig.markup,
+  //     } satisfies InvoiceData;
+
+  //     algaEntries[swoStatement.id!] = newInvoiceData;
+  //     storage.put(STORAGE_NAMESPACE, STORAGE_KEY, algaEntries);
+  //   }
+  // }
 }
