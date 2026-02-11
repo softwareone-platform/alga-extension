c9310544850612a04dde4b92900d07c74ac06023
Jacek Kruzel <jacek.kruzel@gmail.com>
2026-02-11 09:54:27 +0100

    21

--- extension/src/ui/main.tsx
+++ extension/src/ui/main.tsx
@@ -12,31 +12,19 @@ import "./index.css";
 import "@alga-psa/ui-kit/theme.css";
 import { backendClient } from "./lib/alga/url.js";
 import { UserResponseBody } from "@/shared/user";
+import { IframeBridge } from "@alga-psa/extension-iframe-sdk";
 
-//idt:TKN-3140-4844:hUOoIJsnPNBU4MeruvvLDjcYMboih3al2WXyEnY4IeTpZCF1xhex7p1qNPZVCD4b
-//idt:TKN-2515-5802:gcOsB36nFewgcEXVStNz6n9QsfzPz5nkZaNVW0WWl1VBjTttwUYEBFn8kA9lmnnc
-//idt:TKN-3610-0872:IzD4V5gC9T6dBmpMWrZ60eIkzzbzb9OjliIWdrA2Xs2IXU4umyY0e62x7NHOhRgx
 
-//idt:TKN-8557-7823:Rv3ltKu4js3bVvR6Ok6n0JmIfruCTusirs1nI1UDF3T4AzuiHPPkuMG90gHAsNrR
+const bridge = new IframeBridge({ devAllowWildcard: true });
 
-// console.log(ALGA_API_URL, ALGA_API_KEY);
+export async function proxyGet<T>(route: string): Promise<T> {
+  const bytes = await bridge.uiProxy.callRoute(route);
+  const text = new TextDecoder().decode(bytes);
+  return text ? JSON.parse(text) : ({} as T);
+}
 
-//https://portal.s1.live/commerce/subscriptions/SUB-5390-6511-9769
-
-// (async () => {
-//   const eid = window.location.host.split(".")[0];
-//   try {
-//     const url = `/api/ext/${eid}/handler`;
-//     const res = await fetch(url);
-//     const data = await res.json();
-
-//     console.log(data);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// })();
-
-backendClient.get<UserResponseBody>("/user").then(({ data: { userType } }) => {
+proxyGet<UserResponseBody>("/user").then(({ userType }) => {
+  console.log("From proxy", userType);
   const router = createBrowserRouter([
     {
       path: "/",
@@ -64,4 +52,8 @@ backendClient.get<UserResponseBody>("/user").then(({ data: { userType } }) => {
       </QueryClientProvider>
     </StrictMode>
   );
+});
+
+backendClient.get<UserResponseBody>("/user").then(({ data: { userType } }) => {
+  console.log("From backend", userType);
 });
